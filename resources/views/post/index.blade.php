7c1424d8c77428757a7acc321042068c2beaba3c
taku-tech <59305697+taku-tech@users.noreply.github.com>
2020-08-26 17:53:38 +0900

    Add サインアウトボタン

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -5,6 +5,13 @@
 @section('content')
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 
+  <a href="{{ route('logout') }}" onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+    サインアウト<br/>
+  </a>
+  <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+  {{ csrf_field() }}
+  </form>
+
   <p>このページは仮のトップページです。</p>
   <a href="#" class="btn btn-primary">仮のボタンです</a>
 @endsection
