33e912835498b52df4d1a557ff5ac8245bffe4d8
taku-tech <59305697+taku-tech@users.noreply.github.com>
2020-08-27 17:21:25 +0900

    Add 投稿機能の作成

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,12 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
+use Auth;
+use Validator;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
+    public __construct()
+    {
+      $this->middleware('auth');
+    }
+
     public function index()
     {
       return view('post/index');
+    }
+
+    public function new()
+    {
+      return view('post/new');
+    }
+
+    public function store(Request $request)
+    {
+      $validator = Validator::make($request->all() , ['caption' => 'required|max:255', 'photo' => 'required']);
+      if($validator->fails())
+      {
+        return redirect()->back()->withErrors($validator->errors())->withInput();
+      }
+
+      $post = new Post;
+      $post->caption = $request->caption;
+      $post->user_id = Auth::user()->id;
+      $post->save();
+      $request->photo->storeAs('public/post_images', $post->id . '.jpg');
+      return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,11 @@
 */
 
 Route::get('/', 'PostsController@index');
-
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
-
+//Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'PostsController@index');
 Route::get('/users/edit', 'UsersController@edit');
 ROute::post('/users/update', 'UsersController@update');
 Route::get('/users/{user_id}', 'UsersController@show');
+Route::get('/posts/new', 'PostsController@new')->name('new');
+Route::post('/posts', 'PostsController@store');
