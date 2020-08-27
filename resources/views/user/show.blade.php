84e5065179c927122613801c5e60737a2ac500d3
taku-tech <59305697+taku-tech@users.noreply.github.com>
2020-08-27 14:29:35 +0900

    Add ログイン中のユーザのみ表示することを指定

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -17,11 +17,11 @@
     <div class="col-md-8">
       <div class="row">
         <h1>{{ $user->name }}</h1>
-
+        @if ($user->id == Auth::user()->id)
           <a class="btn btn-outline-dark common-btn edit-profile-btn" href="/users/edit">プロフィールを編集</a>
           <a class="btn btn-outline-dark common-btn edit-profile-btn" rel="nofollow" data-method="POST" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ログアウト</a>
           <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">{{ csrf_field() }}</form>
-
+        @endif
       </div>
       <div class="row">
         <p>
